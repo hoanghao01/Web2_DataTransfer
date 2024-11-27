a1f705e8577a4245bc9c38d798c832aa68735b79
hoanghao01 <haongnguyen6345@gmail.com>
2024-11-27 17:03:25 +0700

    add data.php

---
+++ data.php
@@ -0,0 +1,30 @@
+<!-- data.php -->
+<?php 
+
+    $arrLoaiSP = ["Nike", "Adidas", "Puma", "Mizuno", "Kamito", "Zocker", "Ascis"];
+    $arrDSSP = [
+        ['MaSP' => "2024001", "TenSP" => "Nike Vapor 14", "GiaBan" => "2.500.000", "URL" => "./images/nike/nike1.webp", "LoaiSP" => "Nike"],
+        ['MaSP' => "2024002", "TenSP" => "Nike Vapor 15", "GiaBan" => "2.600.000", "URL" => "./images/nike/nike2.webp", "LoaiSP" => "Nike"],
+        ['MaSP' => "2024003", "TenSP" => "Nike Vapor 16", "GiaBan" => "2.700.000", "URL" => "./images/nike/nike3.webp", "LoaiSP" => "Nike"],
+        ['MaSP' => "2024004", "TenSP" => "Adidas Predator 1", "GiaBan" => "2.200.000", "URL" => "./images/adidas/adidas1.webp", "LoaiSP" => "Adidas"],
+        ['MaSP' => "2024005", "TenSP" => "Adidas Predator 2", "GiaBan" => "2.300.000", "URL" => "./images/adidas/adidas2.webp", "LoaiSP" => "Adidas"],
+        ['MaSP' => "2024006", "TenSP" => "Adidas Predator 3", "GiaBan" => "2.400.000", "URL" => "./images/adidas/adidas3.webp", "LoaiSP" => "Adidas"],
+        ['MaSP' => "2024007", "TenSP" => "Puma Future 21", "GiaBan" => "2.000.000", "URL" => "./images/puma/puma1.webp", "LoaiSP" => "Puma"],
+        ['MaSP' => "2024008", "TenSP" => "Puma Future 22", "GiaBan" => "2.100.000", "URL" => "./images/puma/puma2.webp", "LoaiSP" => "Puma"],
+        ['MaSP' => "2024009", "TenSP" => "Puma Future 23", "GiaBan" => "2.200.000", "URL" => "./images/puma/puma3.webp", "LoaiSP" => "Puma"],
+        ['MaSP' => "2024010", "TenSP" => "Mizuno Morelia X", "GiaBan" => "2.300.000", "URL" => "./images/mizuno/mizuno1.webp", "LoaiSP" => "Mizuno"],
+        ['MaSP' => "2024011", "TenSP" => "Mizuno Morelia Y", "GiaBan" => "2.400.000", "URL" => "./images/mizuno/mizuno2.webp", "LoaiSP" => "Mizuno"],
+        ['MaSP' => "2024012", "TenSP" => "Mizuno Morelia Z", "GiaBan" => "2.500.000", "URL" => "./images/mizuno/mizuno3.webp", "LoaiSP" => "Mizuno"],
+        ['MaSP' => "2024013", "TenSP" => "Kamito Legend 19", "GiaBan" => "2.100.000", "URL" => "./images/kamito/kamito1.webp", "LoaiSP" => "Kamito"],
+        ['MaSP' => "2024014", "TenSP" => "Kamito Legend 20", "GiaBan" => "2.200.000", "URL" => "./images/kamito/kamito2.webp", "LoaiSP" => "Kamito"],
+        ['MaSP' => "2024015", "TenSP" => "Kamito Legend 21", "GiaBan" => "2.300.000", "URL" => "./images/kamito/kamito3.webp", "LoaiSP" => "Kamito"],
+        ['MaSP' => "2024016", "TenSP" => "Zocker Inspire", "GiaBan" => "2.400.000", "URL" => "./images/zocker/zocker1.webp", "LoaiSP" => "Zocker"],
+        ['MaSP' => "2024017", "TenSP" => "Zocker Legend", "GiaBan" => "2.500.000", "URL" => "./images/zocker/zocker2.webp", "LoaiSP" => "Zocker"],
+        ['MaSP' => "2024018", "TenSP" => "Zocker Partial", "GiaBan" => "2.200.000", "URL" => "./images/zocker/zocker3.webp", "LoaiSP" => "Zocker"],
+        ['MaSP' => "2024019", "TenSP" => "Ascis Gel 1.1", "GiaBan" => "2.600.000", "URL" => "./images/ascis/ascis1.webp", "LoaiSP" => "Ascis"],
+        ['MaSP' => "2024020", "TenSP" => "Ascis Gel 2.0", "GiaBan" => "2.650.000", "URL" => "./images/ascis/ascis2.jpg", "LoaiSP" => "Ascis"],
+        ['MaSP' => "2024021", "TenSP" => "Ascis Gel 3", "GiaBan" => "2.700.000", "URL" => "./images/ascis/ascis3.jpg", "LoaiSP" => "Ascis"]
+    ];
+    
+?>
+
